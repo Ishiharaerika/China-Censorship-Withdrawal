a1d7384055252a8f9546675c0f48a54ffbb48e53
石原枝林下 <87614796+Ishiharaerika@users.noreply.github.com>
2022-08-04 16:10:56 +0900

    Update Wechat.meta.js

--- Adguard/Wechat.meta.js
+++ Adguard/Wechat.meta.js
@@ -1,12 +1,12 @@
-==UserScript==
-@name					WechatAdblock
-@description			Disable part of Wechat ads.
-@version				1.0
-@downloadURL			https://raw.githubusercontent.com/Ishiharaerika/China-Censorship-Withdrawal/main/Adguard/Wechat.meta.js
-@updateURL				https://raw.githubusercontent.com/Ishiharaerika/China-Censorship-Withdrawal/main/Adguard/Wechat.meta.js
-@include				^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad
+// ==UserScript==
+// @name					WechatAdblock
+// @description			Disable part of Wechat ads.
+// @version				1.0
+// @downloadURL			https://raw.githubusercontent.com/Ishiharaerika/China-Censorship-Withdrawal/main/Adguard/Wechat.meta.js
+// @updateURL				https://raw.githubusercontent.com/Ishiharaerika/China-Censorship-Withdrawal/main/Adguard/Wechat.meta.js
+// @include				^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad
 
-==/UserScript==
+// ==/UserScript==
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
